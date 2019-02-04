46b92d55a2641438b658f59152e58b5db8a37f2e
shannonay0103 <43756247+shannonay0103@users.noreply.github.com>
2019-02-04 12:36:17 -0500

    Add files via upload

---
+++ Lesson 3/problem2.py
@@ -0,0 +1,4 @@
+for x in range (1001,-1,-1):
+	print(x)
+
+print('Blast off!')
---
+++ Lesson 3/problem4.py
@@ -0,0 +1,3 @@
+for t in range (0,51):
+	a= 10000 * (2.718) ** (0.07 * t)
+	print('After ' + str(t) + ' years, I will have $' + str(a))
